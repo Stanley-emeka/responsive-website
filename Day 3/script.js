c7f9bc453e5c2eafb1a24693cde6cd37a709f821
Emeka <emekaudigwe@gmail.com>
2026-08-31 04:55:23 +0100

    Update Day 3 website

--- Day 3/script.js
+++ Day 3/script.js
@@ -4,3 +4,20 @@ toggleNav.addEventListener('click', () =>{
     toggleNav.classList.toggle('active')
     navOpen.classList.toggle('active')
 });
+
+
+function openFooter(pf){
+let demo = pf.parentElement.querySelector(".demo");
+
+ if (pf.textContent === "+"){
+ demo.style.display = "block"
+  pf.textContent = "-"
+  
+  
+ }
+ 
+ else if (pf.textContent === "-"){
+  pf.textContent = "+"
+  demo.style.display = "none"
+ }
+}
